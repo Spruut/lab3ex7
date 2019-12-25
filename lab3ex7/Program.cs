496b07301c3127f7be812a77c4840730d42a8395
Ких Даниил <danilkih@gmail.com>
2019-12-25 10:12:17 +0200

    Добавьте файлы проекта.

---
+++ lab3ex7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lab3ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("Enter number N for Fibonacci series ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{n} element of Fibonacci series - {Fibonacci(n)}");
+            Console.ReadKey();
+            Program.Main(args);
+        }
+        static int Fibonacci(int n)
+        {
+            if (n == 1)
+                return 1;
+            if (n == 2)
+                return 2;
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
